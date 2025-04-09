030ebb9faa8aadf6e57e4d907f0fced9c42d800c
Candace Savonen <cansav09@gmail.com>
2025-04-09 12:40:20 -0400

    skip_on_cran

--- tests/testthat/test-citation.R
+++ tests/testthat/test-citation.R
@@ -1,4 +1,5 @@
 test_that("citations pulled", {
+  testthat::skip_on_cran()
   paper_cite_link <- "https://scholar.google.com/scholar?cites=6140457238337460780"
 
   papers_cited_df <- get_citation_count(paper_cite_link)
