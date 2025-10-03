de6b86879a60d4aeec414fb55e0134ebafacf0c7
Kate Isaac <41767733+kweav@users.noreply.github.com>
2025-10-03 15:00:18 -0400

    Update test-citation.R to mimic other tests with creds

--- tests/testthat/test-citation.R
+++ tests/testthat/test-citation.R
@@ -1,17 +1,28 @@
-test_that("citations pulled", {
+auth_tokens <-
+  c(
+    Sys.getenv("METRICMINER_GOOGLE_REFRESH"),
+    Sys.getenv("METRICMINER_GOOGLE_ACCESS")
+  )
 
-  testthat::skip_on_cran()
-  
-  auth_from_secret("google",
+if (all(!(auth_tokens == ""))) {
+
+  test_that("citations pulled", {
+    # Interactively create google token
+    auth_from_secret("google",
       refresh_token = Sys.getenv("METRICMINER_GOOGLE_REFRESH"),
       access_token = Sys.getenv("METRICMINER_GOOGLE_ACCESS"),
       cache = FALSE,
       in_test = TRUE
     )
   
-  paper_cite_link <- "https://scholar.google.com/scholar?cites=6140457238337460780"
+    paper_cite_link <- "https://scholar.google.com/scholar?cites=6140457238337460780"
 
-  papers_cited_df <- get_citation_count(paper_cite_link)
+    papers_cited_df <- get_citation_count(paper_cite_link)
 
-  testthat::expect_named(papers_cited_df, c('original_paper', 'cite_titles', 'links'))
-})
+    testthat::expect_named(papers_cited_df, c('original_paper', 'cite_titles', 'links'))
+  
+  })
+
+} else {
+  message("testthat tests skipped because no auth detected")
+}
