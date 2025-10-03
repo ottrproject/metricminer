29f43ab1dfe27da33de7f95050b1c23c9a603f41
Kate Isaac <41767733+kweav@users.noreply.github.com>
2025-10-03 14:24:58 -0400

    Update test-citation.R to give it google token

--- tests/testthat/test-citation.R
+++ tests/testthat/test-citation.R
@@ -1,4 +1,12 @@
 test_that("citations pulled", {
+
+  auth_from_secret("google",
+      refresh_token = Sys.getenv("METRICMINER_GOOGLE_REFRESH"),
+      access_token = Sys.getenv("METRICMINER_GOOGLE_ACCESS"),
+      cache = FALSE,
+      in_test = TRUE
+    )
+  
   paper_cite_link <- "https://scholar.google.com/scholar?cites=6140457238337460780"
 
   papers_cited_df <- get_citation_count(paper_cite_link)
